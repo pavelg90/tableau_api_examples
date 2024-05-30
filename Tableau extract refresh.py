0e3d42b92fe27d0ef14458a86c2d2ee87cd679d0
Pavel Gomon <pavelg90@gmail.com>
2024-05-30 12:54:51 +0300

    Update Tableau extract refresh.py

--- Tableau extract refresh.py
+++ Tableau extract refresh.py
@@ -1,36 +1,73 @@
 import tableauserverclient as TSC
 
-server_name = "<XX_YY>.online.tableau.com"    # Name or IP address of your installation of Tableau Server
+server_name = "https://<XX_YY>.online.tableau.com"    # Name or IP address of your installation of Tableau Server
 site_url_id = "<XX_YY>"    			# Site (subpath) to sign in to. An empty string is used to specify the default site.
 
 # For Personal Access Token sign in
 personal_access_token_name = "<XX_YY>"        # Name of the personal access token.
 personal_access_token_secret = "<XX_YY>"   	# Value of the token.
 
-# Data source name for extract refresh
-ds_name = '<XX_YY>'
-
-tableau_auth = TSC.PersonalAccessTokenAuth(personal_access_token_name, 
-				           personal_access_token_secret,
-				           site_url_id)
-
+tableau_auth = TSC.PersonalAccessTokenAuth(
+    personal_access_token_name,
+	personal_access_token_secret,
+	site_url_id
+)
 
 server = TSC.Server(server_name, use_server_version=True)
-
 server.auth.sign_in(tableau_auth)
 
 
+"""
+	Find DS by tag
+"""
+ds_tag = 'NBA'
 with server.auth.sign_in(tableau_auth):
-    all_datasources, pagination_item = server.datasources.get()
+	# Get all DSs from Server
+	all_datasources, pagination_item = server.datasources.get()
+	while True:
+		try:
+			datasources, pagination_item = server.datasources.get(
+				TSC.RequestOptions(
+					pagenumber=pagination_item.page_number + 1
+				)
+			)
+			all_datasources.extend(datasources)
+		except:
+			break
 
-    for ds in all_datasources:
-    	if ds.name == ds_name:
-    		print(f"Found ds: {ds_name}, ID: {ds.id}")
-    		ds_id = ds.id
+	for ds in all_datasources:
+		if ds_tag in ds.tags:
+			print(f"Found ds: {ds.name}, ID: {ds.id}")
 
-    		ds_to_update = server.datasources.get_by_id(ds_id)
-    		refreshed_ds = server.datasources.refresh(ds_to_update)
-    		print(f"Sent DS refresh extract now")
-                            break
+			ds_to_update = server.datasources.get_by_id(ds.id)
+			refreshed_ds = server.datasources.refresh(ds_to_update)
+			print(f"Sent DS refresh extract now")
+
+
+"""
+	Find DS by Name
+"""
+ds_names = ['something', 'something 2']
+with server.auth.sign_in(tableau_auth):
+	# Get all DSs from Server
+	all_datasources, pagination_item = server.datasources.get()
+	while True:
+		try:
+			datasources, pagination_item = server.datasources.get(
+				TSC.RequestOptions(
+					pagenumber=pagination_item.page_number + 1
+				)
+			)
+			all_datasources.extend(datasources)
+		except:
+			break
+
+	for ds in all_datasources:
+		if ds.name in ds_names:
+			print(f"Found ds: {ds.name}, ID: {ds.id}")
+
+			ds_to_update = server.datasources.get_by_id(ds.id)
+			refreshed_ds = server.datasources.refresh(ds_to_update)
+			print(f"Sent DS refresh extract now")
 
 
